406117629161b894b6b34a1a384909184365c388
Carlos Emanuel <127751547+CarlosKarlos@users.noreply.github.com>
2024-04-11 20:23:52 -0300

    continuando

--- exercicios/modelo02/script.js
+++ exercicios/modelo02/script.js
@@ -1,6 +1,22 @@
-var ano= window.document.querySelector('input#numberY')
-var anos= Number(ano.value)
 
-var agora= new Date()
-var anoatual= agora.getFullYear()
-log.console(anoatual)
+
+  // Função para verificar a opção selecionada
+  function verificarGenero() {
+   
+    var selectGenero = document.getElementById("genero");
+    // Obtém o valor da opção selecionada
+    var generoSelecionado = selectGenero.value;
+
+    // Verifica o valor selecionado e faz suas ações
+    if (generoSelecionado === "homem") {
+      console.log("Usuário selecionou Homem");
+      // Faça algo quando o usuário selecionar "Homem"
+    } else if (generoSelecionado === "mulher") {
+      console.log("Usuário selecionou Mulher");
+      // Faça algo quando o usuário selecionar "Mulher"
+    } else {
+      console.log("Nenhuma opção selecionada");
+      // Faça algo quando nenhuma opção for selecionada
+    }
+  }
+
