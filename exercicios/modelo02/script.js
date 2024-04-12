18d62f68634a387304d427af60ca845d66b543d5
Carlos Emanuel <127751547+CarlosKarlos@users.noreply.github.com>
2024-04-12 16:19:33 -0300

    atualização

--- exercicios/modelo02/script.js
+++ exercicios/modelo02/script.js
@@ -1,22 +1,87 @@
 
-
-  // Função para verificar a opção selecionada
+//função para verificar idade e genero selecionado
+  
   function verificarGenero() {
-   
+    var res= document.querySelector('div#res')
     var selectGenero = document.getElementById("genero");
-    // Obtém o valor da opção selecionada
+    var txtano = document.querySelector('input#txtano')
     var generoSelecionado = selectGenero.value;
+    var dataN= Number(txtano.value)
+    var agora = new Date()
+    var anoatual = Number(agora.getFullYear())
+    var idade = anoatual - dataN
+    
+    
+    switch (generoSelecionado) {
+      case "homem":
+          switch (true) {
+              case idade < 10 && idade >0 :
+                res.innerHTML = `você  tem ${idade} anos e está na na infância!`;
+                res.src = "";
+                res.style.color = "#18E100"
+                  break;
+              case idade >= 11 && idade <= 18:
+                res.innerHTML = `você tem ${idade} anos e está na adolescência!`;
+                res.src = "";
+                res.style.color = "#18E100"
+                  break;
+              case idade >= 19 && idade <= 25:
+                res.innerHTML = `você tem ${idade} anos e é um jovem adulto!`;
+                res.src = "";
+                res.style.color = "#18E100"
+                  break;
+              case idade >= 26 && idade <= 45:
+                res.innerHTML =  `você tem ${idade} anos e é um adulto!`;
+                res.src = '';
+                res.style.color = "#18E100"
+                  break; //
+             
+              case idade>= 46 && idade <= 125:
+                res.innerHTML = `você tem ${idade} anos e é um senhor de idade!`;
+                res.style.color = "#18E100"
 
-    // Verifica o valor selecionado e faz suas ações
-    if (generoSelecionado === "homem") {
-      console.log("Usuário selecionou Homem");
-      // Faça algo quando o usuário selecionar "Homem"
-    } else if (generoSelecionado === "mulher") {
-      console.log("Usuário selecionou Mulher");
-      // Faça algo quando o usuário selecionar "Mulher"
-    } else {
-      console.log("Nenhuma opção selecionada");
-      // Faça algo quando nenhuma opção for selecionada
-    }
+                 default:
+                res.innerHTML= `coloque uma idade válida pois é impossivel que tenha ${idade} anos!`
+                res.style.color = "#E0130D";
+                  break;
+          }
+          break;
+          case "mulher":
+            switch (true) {
+                case idade < 10 && idade >0 :
+                    res.innerHTML = `você tem ${idade} anos e está na infância!`;
+                    res.src = "";
+                    res.style.color = "#18E100"
+                    break;
+                case idade >= 11 && idade <= 18:
+                    res.innerHTML = `você tem ${idade} anos e está na adolescência!`;
+                    res.src = "";
+                    res.style.color = "#18E100"
+                    break;
+                case idade >= 19 && idade <= 25:
+                    res.innerHTML = `você tem ${idade} anos e é uma jovem adulta!`;
+                    res.src = "";
+                    res.style.color = "#18E100"
+                    break;
+                case idade >= 26 && idade <= 45:
+                    res.innerHTML =  `você tem ${idade} anos e é uma adulta!`;
+                    res.src = '';
+                    res.style.color = "#18E100"
+                    break;
+                
+                    case idade>= 46 && idade <= 125:
+                      res.innerHTML = `você tem ${idade} anos e é um senhora de idade!`;
+                      res.style.color = "#18E100"
+                
+                    default:
+                      res.innerHTML= `coloque uma idade válida pois é impossivel que tenha ${idade} anos!`
+                      res.style.color = "#E0130D";
+                        break;
+            }
+            break;
+      default:
+          res.innerHTML='selecione e coloque a idade!'
+          break;
+  }
   }
 
