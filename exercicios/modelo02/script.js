546b48c7975e313da565c8a6311fc81bd7d0cc75
Carlos Emanuel <127751547+CarlosKarlos@users.noreply.github.com>
2024-04-17 12:11:26 -0300

    atualizaçoes

--- exercicios/modelo02/script.js
+++ exercicios/modelo02/script.js
@@ -10,38 +10,39 @@
     var agora = new Date()
     var anoatual = Number(agora.getFullYear())
     var idade = anoatual - dataN
-    
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
     
     switch (generoSelecionado) {
       case "homem":
           switch (true) {
               case idade < 10 && idade >0 :
-                res.innerHTML = `você  tem ${idade} anos e está na na infância!`;
+                res.innerHTML =  `você é um homem de ${idade} anos!`;
                 res.src = "";
                 res.style.color = "#18E100"
                   break;
               case idade >= 11 && idade <= 18:
-                res.innerHTML = `você tem ${idade} anos e está na adolescência!`;
+                res.innerHTML =  `você é um homem de ${idade} anos!`;
                 res.src = "";
                 res.style.color = "#18E100"
                   break;
               case idade >= 19 && idade <= 25:
-                res.innerHTML = `você tem ${idade} anos e é um jovem adulto!`;
+                res.innerHTML =  `você é um homem de ${idade} anos!`;
                 res.src = "";
                 res.style.color = "#18E100"
                   break;
               case idade >= 26 && idade <= 45:
-                res.innerHTML =  `você tem ${idade} anos e é um adulto!`;
+                res.innerHTML =  `você é um homem de ${idade} anos!`;
                 res.src = '';
                 res.style.color = "#18E100"
                   break; //
              
               case idade>= 46 && idade <= 125:
-                res.innerHTML = `você tem ${idade} anos e é um senhor de idade!`;
+                res.innerHTML =  `você é um homem de ${idade} anos!`;
                 res.style.color = "#18E100"
-
+                break
                  default:
-                res.innerHTML= `coloque uma idade válida pois é impossivel que tenha ${idade} anos!`
+                res.innerHTML= `coloque uma idade válida!`
                 res.style.color = "#E0130D";
                   break;
           }
@@ -49,39 +50,39 @@
           case "mulher":
             switch (true) {
                 case idade < 10 && idade >0 :
-                    res.innerHTML = `você tem ${idade} anos e está na infância!`;
+                    res.innerHTML =  `você é uma mulher de ${idade} anos!`;
                     res.src = "";
                     res.style.color = "#18E100"
                     break;
                 case idade >= 11 && idade <= 18:
-                    res.innerHTML = `você tem ${idade} anos e está na adolescência!`;
+                    res.innerHTML = `você é uma mulher de ${idade} anos!`;
                     res.src = "";
                     res.style.color = "#18E100"
                     break;
                 case idade >= 19 && idade <= 25:
-                    res.innerHTML = `você tem ${idade} anos e é uma jovem adulta!`;
+                    res.innerHTML = `você é uma mulher de ${idade} anos!`;
                     res.src = "";
                     res.style.color = "#18E100"
                     break;
                 case idade >= 26 && idade <= 45:
-                    res.innerHTML =  `você tem ${idade} anos e é uma adulta!`;
+                    res.innerHTML =  `você é uma mulher de ${idade} anos!`;
                     res.src = '';
                     res.style.color = "#18E100"
                     break;
                 
                     case idade>= 46 && idade <= 125:
-                      res.innerHTML = `você tem ${idade} anos e é um senhora de idade!`;
+                      res.innerHTML =`você é uma mulher de ${idade} anos!`;
                       res.style.color = "#18E100"
+                      break
                 
                     default:
-                      res.innerHTML= `coloque uma idade válida pois é impossivel que tenha ${idade} anos!`
+                      res.innerHTML= `coloque uma idade válida!`
                       res.style.color = "#E0130D";
                         break;
             }
             break;
       default:
-          res.innerHTML='selecione e coloque a idade!'
-          break;
+        
   }
   }
 
