7fb4ed1f856663b47464002214526e38c4fe84fe
Carlos Emanuel <127751547+CarlosKarlos@users.noreply.github.com>
2024-04-18 18:37:05 -0300

    estudos

---
+++ exercicios/modelo02/estudo_13/estudo_13.js
@@ -0,0 +1,51 @@
+/*
+
+console.log('passo 1')
+console.log('passo 2')
+console.log('passo 3')
+console.log('passo 4')
+
+
+
+
+var c = 1
+
+while(c <= 5){
+
+    console.log(`passo ${c}`)
+    c++
+
+}
+
+
+/*
+var c = 1
+do{
+    console.log(`passo ${c}`)
+    c++
+
+
+}
+
+
+while(c <= 5)
+
+
+
+
+
+for(var c=1;c <= 10;c++){
+
+
+    console.log(`passo ${c}`)
+
+}
+
+*/
+
+console.log('vai começar...')
+for(var c =1;c <= 10 ; c++){
+
+console.log(c)
+}
+console.log('fim!')
